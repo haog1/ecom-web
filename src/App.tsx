9201ce1abbaaf03cad68306d93365c0250a22062
Tony Gao <gaohaoqian@gmail.com>
2021-01-23 22:57:17 +1100

    feat: add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { NotFound } from 'pages/404';
 import { HomePage } from 'pages/home';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Login } from 'pages/login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={HomePage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
