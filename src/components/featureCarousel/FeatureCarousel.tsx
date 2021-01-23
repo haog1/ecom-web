37e3bcf43250a11e688c0e601176ba121c71afd8
Tony Gao <gaohaoqian@gmail.com>
2021-01-23 19:58:52 +1100

    feat: add featured carousel

--- src/components/featureCarousel/FeatureCarousel.tsx
+++ src/components/featureCarousel/FeatureCarousel.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import styles from './FeatureCarousel.module.css';
+import { Image, Carousel } from 'antd';
+import SlideImage1 from 'assets/images/mock/carousel_1.jpg';
+import SlideImage2 from 'assets/images/mock/carousel_2.jpg';
+import SlideImage3 from 'assets/images/mock/carousel_3.jpg';
 
 export const FeatureCarousel: React.FC = () => {
-  return <div></div>;
+  return (
+    <Carousel
+      autoplay
+      adaptiveHeight={false}
+      draggable={true}
+      className={styles.slider}
+    >
+      <Image src={SlideImage1} />
+      <Image src={SlideImage2} />
+      <Image src={SlideImage3} />
+    </Carousel>
+  );
 };
