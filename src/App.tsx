1de9292048b0c81989824d03cbe0fea39ca81fc9
Tony Gao <gaohaoqian@gmail.com>
2021-01-23 19:31:09 +1100

    style: remove unnecessary imports and clean imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import styles from 'assets/css/Global.module.css';
-import { AppHeader, AppFooter, SideMenu } from 'components';
-import { Row, Col } from 'antd';
 import { HomePage } from 'pages/Home';
 
 function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import './index.css';
 import 'antd/dist/antd.css';
+import 'assets/css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
